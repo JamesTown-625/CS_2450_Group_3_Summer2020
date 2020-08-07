89fe2d5d77b237bcf953b7f3b97c7eda1eed9a8e
Tanner Wood <tanner.wood@vivint.com>
2020-08-07 09:28:53 -0600

    sped run up by 20x (not exaggerating lol)

--- uvsim/src/App2.js
+++ uvsim/src/App2.js
@@ -169,24 +169,18 @@ export default class App2 extends React.Component {
     //   running: true
     // }));
 
-    this.setState({running: true})
+    this.setState({ running: true });
     setTimeout(() => {
-      
-        console.log("okay tryyyyy");
-        while (this.state.running == true) {
-          console.log("Running is true");
-          let line = memory[this.state.program_counter].machine_language_line;
-          console.log(
-            `loop iteration  PC:${this.state.program_counter} CURRENT LINE:${line}`
-          );
-          this.executeOperation();
-        }
-       
-      
-    }, 1000);
-    
-
-    
+      console.log("okay tryyyyy");
+      while (this.state.running == true) {
+        console.log("Running is true");
+        let line = memory[this.state.program_counter].machine_language_line;
+        console.log(
+          `loop iteration  PC:${this.state.program_counter} CURRENT LINE:${line}`
+        );
+        this.executeOperation();
+      }
+    }, 50);
   };
 
   //Open Help Window
