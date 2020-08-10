5c4fc7d2795e82eb2f43d5f9a51955c792716bdf
Tanner Wood <tanner.wood@vivint.com>
2020-08-09 22:49:49 -0600

    filled out the test programs

--- uvsim/src/Components/HelpScreen.js
+++ uvsim/src/Components/HelpScreen.js
@@ -28,49 +28,48 @@ const useStyles = makeStyles({
 const HelpScreen = (props) => {
   const classes = useStyles();
   let template = [
-    `@ ORIG x0000\n0011000000000000\n@ add decimal 10 to register 1 (immediate mode\n0001001001101010\n@ add the contents of register 1 to register 2 (not immediate mode)\n0001010001000010\n@ add decimal 10 to register 4 (immediate mode)\n0001100100101010\n@ subtract decimal 5 to register 1 (immediate mode)\n0002001001100101\n@ subtract the value of register 1 from register 2 (not immediate mode)\n0002010001000010\n@ multiply register 2 by 2 (immediate mode)\n0003010010100010\n@ divide register 2 by 2 (not immediate mode)\n0004010010100010\n@ mod register 2 by 3 (immediate mode)\n0005010010100011\n@ register 1 to the power of the value of register 3 (not immediate mode)\n0006001001000011\n@ TRAP- HALT\n1111000000100101`,
-    `00110000000000000000\n0000000000000000\n1010101010101010\n0101010100110101`,
-    `00110001111000000000\n0000111110000000\n1010101010101010\n0101010100110101`,
-    `00110001111000111000\n0000111110001100\n1010101010101010\n0101010100000101`
-  ]
-  const [selectedTemplate, setSelectedTemplate] = useState([template[0]])
-  const [selectedOne, setSelectedOne] = useState("contained")
-  const [selectedTwo, setSelectedTwo] = useState("outlined")
-  const [selectedThree, setSelectedThree] = useState("outlined")
-  const [selectedFour, setSelectedFour] = useState("outlined")
+    `@ ORIG x0000\n0011000000000000\n\n@ add decimal 10 to register 1 (immediate mode\n0001001001101010\n@ add the contents of register 1 to register 2 (not immediate mode)\n0001010001000010\n\n@ add decimal 10 to register 4 (immediate mode)\n0001100100101010\n@ subtract decimal 5 to register 1 (immediate mode)\n0002001001100101\n@ subtract the value of register 2 from register 1 (not immediate mode)\n0002010001000010\n\n@ multiply register 2 by 2 (immediate mode)\n0003010010100010\n@ divide register 2 by 2 (not immediate mode)\n0004010010100010\n\n@ mod register 2 by 3 (immediate mode)\n0005010010100011\n@ register 1 to the power of the value of register 3 (not immediate mode)\n0006001001000011\n\n@ TRAP- HALT\n1111000000100101`,
+    `@ ORIG x0000\n0011000000000000\n@ add decimal 10 to register 1 (immediate mode)\n0001001001101010\n\n@ branch past the following TRAP HALT command if r1 is positive\n0000001000000010\n@ TRAP- HALT\n1111000000100101\n\n@ add decimal 10 to register 1 (immediate mode)\n0001001001101010\n@ TRAP- HALT\n1111000000100101`,
+    `@ ORIG x0000\n0011000000000000\n@ add decimal 10 to register 1 (immediate mode)\n0001001001101010\n\n@ store the value of register 1 to memory address 32\n0011001000100000\n@ load the value from memory address 32 and store it in register 2\n0010010000100000\n\n@ TRAP- HALT\n1111000000100101`,
+    // `00110001111000111000\n0000111110001100\n1010101010101010\n0101010100000101`,
+  ];
+  const [selectedTemplate, setSelectedTemplate] = useState([template[0]]);
+  const [selectedOne, setSelectedOne] = useState("contained");
+  const [selectedTwo, setSelectedTwo] = useState("outlined");
+  const [selectedThree, setSelectedThree] = useState("outlined");
+  // const [selectedFour, setSelectedFour] = useState("outlined");
 
   const chooseTemplateOne = () => {
-    setSelectedTemplate(template[0])
-    setSelectedOne("contained")
-    setSelectedTwo("outlined")
-    setSelectedThree("outlined")
-    setSelectedFour("outlined")
-    
-  }
+    setSelectedTemplate(template[0]);
+    setSelectedOne("contained");
+    setSelectedTwo("outlined");
+    setSelectedThree("outlined");
+    // setSelectedFour("outlined");
+  };
 
   const chooseTemplateTwo = () => {
-    setSelectedTemplate(template[1])
-    setSelectedOne("outlined")
-    setSelectedTwo("contained")
-    setSelectedThree("outlined")
-    setSelectedFour("outlined")
-  }
+    setSelectedTemplate(template[1]);
+    setSelectedOne("outlined");
+    setSelectedTwo("contained");
+    setSelectedThree("outlined");
+    // setSelectedFour("outlined");
+  };
 
   const chooseTemplateThree = () => {
-    setSelectedTemplate(template[2])
-    setSelectedOne("outlined")
-    setSelectedTwo("outlined")
-    setSelectedThree("contained")
-    setSelectedFour("outlined")
-  }
+    setSelectedTemplate(template[2]);
+    setSelectedOne("outlined");
+    setSelectedTwo("outlined");
+    setSelectedThree("contained");
+    // setSelectedFour("outlined");
+  };
 
   const chooseTemplateFour = () => {
-    setSelectedTemplate(template[3])
-    setSelectedOne("outlined")
-    setSelectedTwo("outlined")
-    setSelectedThree("outlined")
-    setSelectedFour("contained")
-  }
+    setSelectedTemplate(template[3]);
+    setSelectedOne("outlined");
+    setSelectedTwo("outlined");
+    setSelectedThree("outlined");
+    // setSelectedFour("contained");
+  };
 
   /*
     @ ORIG x0000
@@ -90,10 +89,34 @@ const HelpScreen = (props) => {
     >
       <Paper className={classes.box}>
         <div className={classes.header}>Need Help? Here's some test code:</div>
-        <Button color="primary" variant={selectedOne} onClick={chooseTemplateOne}>One</Button>
-        <Button color="primary" variant={selectedTwo} onClick={chooseTemplateTwo}>Two</Button>
-        <Button color="primary" variant={selectedThree} onClick={chooseTemplateThree}>Three</Button>
-        <Button color="primary" variant={selectedFour} onClick={chooseTemplateFour}>Four</Button>
+        <Button
+          color="primary"
+          variant={selectedOne}
+          onClick={chooseTemplateOne}
+        >
+          Arithmetic Operations
+        </Button>
+        <Button
+          color="primary"
+          variant={selectedTwo}
+          onClick={chooseTemplateTwo}
+        >
+          Control Flow
+        </Button>
+        <Button
+          color="primary"
+          variant={selectedThree}
+          onClick={chooseTemplateThree}
+        >
+          Loading and Storing
+        </Button>
+        {/* <Button
+          color="primary"
+          variant={selectedFour}
+          onClick={chooseTemplateFour}
+        >
+          Four
+        </Button> */}
         <TextField
           multiline
           rows={20}
